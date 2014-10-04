803aaaa5c05f4f52a783a573268cc73ea026ebc4
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2014-10-04 17:34:42 +0200

    Envoi du mail de réservation.

--- src/main/java/org/picotteland/sales/contexts/SpringWebContext.java
+++ src/main/java/org/picotteland/sales/contexts/SpringWebContext.java
@@ -2,6 +2,7 @@ package org.picotteland.sales.contexts;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -13,5 +14,11 @@ public class SpringWebContext extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:website/").setCachePeriod(0);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Use forward: prefix so that no view resolution is done
+        registry.addViewController("/").setViewName("forward:/index.html");
     }
 }
--- src/main/resources/website/js/app.js
+++ src/main/resources/website/js/app.js
@@ -4,8 +4,8 @@
 
 'use strict';
 
-angular.module('nilsArmoireApp', ['ngAnimate', 'ngTouch'])
-    .controller('MainCtrl', function ($scope, $http) {
+angular.module('nilsArmoireApp', ['ngAnimate', 'ngTouch', 'uz.mailto'])
+    .controller('MainCtrl', function ($scope, $http, $window, Mailto) {
 
         // Categorie pour le menu
         $scope.categories = {};
@@ -43,6 +43,21 @@ angular.module('nilsArmoireApp', ['ngAnimate', 'ngTouch'])
         $scope.showPhoto = function (index) {
             $scope.idx = index;
         };
+
+        // Send eMail from OS client
+        $scope.reserve = function () {
+            var articleRef = $scope.currentCategorie.photos[$scope.idx].ref;
+            var articleTitle = $scope.currentCategorie.photos[$scope.idx].desc.title;
+
+            var recepient = "amandine.daigle@gmail.com";
+            var options = {
+                cc: "gregory.depuille@gmail.com",
+                subject: "Réservation article : " + articleTitle + " [" + articleRef + "] : ",
+                body: "Bonjour,\n\nJe souhaite réserver l'article " + articleTitle + " [" + articleRef +"].\n\nCordialement."
+            };
+
+            $window.location = Mailto.url(recepient, options);
+        }
 
         // Chargement des catégorie de photos
         $http({method: 'GET', url: '/categorie'}).
